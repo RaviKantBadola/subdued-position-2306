3f284a9563c8c2713ca0a38098c9cf42a76732c1
RaviKantBadola <107493045+RaviKantBadola@users.noreply.github.com>
2023-01-18 14:43:57 +0530

    first

---
+++ wishcart/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/react'
+
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <Box bg='#2874f0' w='100%' p={4} color='white'>
+   <img src={"https://i.ibb.co/8z4B4GD/wishcart-low-resolution-logo-color-on-transparent-background-1.png"} width="100px" />
+
+  </Box>
+  )
+}
+
+export default navbar
