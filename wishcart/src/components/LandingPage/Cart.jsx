e8b6b0a2b8756b489bdb55dc4b48f696488266e6
RaviKantBadola <107493045+RaviKantBadola@users.noreply.github.com>
2023-01-20 01:37:52 +0530

    day-3

--- wishcart/src/components/LandingPage/Cart.jsx
+++ wishcart/src/components/LandingPage/Cart.jsx
@@ -1,44 +1,27 @@
-import React from 'react'
-import {
- 
-  Box,
-  Flex,
- 
-} from '@chakra-ui/react';
+import {Box} from "@chakra-ui/react"
+ const navData = [
+  {url:"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/47e5c31ef7dcce1e.png?q=100",text:"Top Offers"},
+  {url:"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/5f8bd5669e19e724.png?q=100",text:"Mobiles & Tablets"},
+  {url:"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/361d53b8725c2d2d.png?q=100",text:"Electronics"},
+  {url:"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/904f3c8e7101408b.png?q=100",text:"TVs & Appliances"},
+  {url:"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/7f7355480c6adc16.png?q=100",text:"Fashion"},
+  {url:"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/f97d6138516056bc.png?q=100",text:"Beauty"},
+  {url:"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/6728ed3cf145562c.png?q=100",text:"Home & Furniture"},
+  {url:"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/007910082ae6355b.png?q=100",text:"Flights"},
+  {url:"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/46376ceed3448aff.png?q=100",text:"Grocery"}
+]
 const Cart = () => {
   return (
-   <div>
-<Box justifyContent={"center"} display={"grid"} boxShadow={"rgba(0, 0, 0, 0.35) 0px 5px 15px;"} >
-<Flex>
-
-  <Box  marginRight={"40px"} marginTop={"10px"} marginBottom={"10px"}><img src={"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/47e5c31ef7dcce1e.png?q=100" }  width={"64px"} height={"64px"}/></Box>
-  <Box  marginRight={"40px"} marginTop={"10px"} marginBottom={"10px"}><img src={"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/5f8bd5669e19e724.png?q=100"} width={"64px"} height={"64px"}/></Box>
-  <Box  marginRight={"40px"} marginTop={"10px"} marginBottom={"10px"}><img src={"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/361d53b8725c2d2d.png?q=100"} width={"64px"} height={"64px"}/></Box>
-  <Box  marginRight={"40px"} marginTop={"10px"} marginBottom={"10px"}><img src={"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/904f3c8e7101408b.png?q=100"} width={"64px"} height={"64px"}/></Box>
-  <Box  marginRight={"40px"} marginTop={"10px"} marginBottom={"10px"}><img src={"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/7f7355480c6adc16.png?q=100"} width={"64px"} height={"64px"}/></Box>
-  <Box  marginRight={"40px"} marginTop={"10px"} marginBottom={"10px"}><img src={"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/f97d6138516056bc.png?q=100"} width={"64px"} height={"64px"}/></Box>
-  <Box  marginRight={"40px"} marginTop={"10px"} marginBottom={"10px"}><img src={"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/6728ed3cf145562c.png?q=100"} width={"64px"} height={"64px"}/></Box>
-  <Box  marginRight={"40px"} marginTop={"10px"} marginBottom={"10px"}> <img src={"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/007910082ae6355b.png?q=100"} width={"64px"} height={"64px"}/></Box>
-  <Box  marginRight={"40px"} marginTop={"10px"} marginBottom={"10px"}><img src={"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/46376ceed3448aff.png?q=100"} width={"64px"} height={"64px"}/></Box>
-  
-</Flex>
-<Box>
-  <Flex>
-  <Box>Top Offer </Box>
-  <Box >Mobiles & Tablets</Box>
-  <Box>Electronics</Box>
-  <Box>TVs & Appliances</Box>
-  <Box>Fashion</Box>
-  <Box>Beauty</Box>
-  <Box>Home & Furniture</Box>
-  <Box>Flights</Box>
-  <Box>Grocery</Box>
-  </Flex>
-</Box>
-</Box>
-
-   </div>
+    <Box style={{display:"flex", justifyContent:"space-evenly",padding:"12px 8px",textAlign:"center",boxShadow:"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",width:"100%"}}>
+      {navData.map(data=>(
+     <Box key={data.text}  >
+      <img src={data.url} alt="nav" style={{width:64,marginLeft:"auto",marginRight:"auto"}}/>
+      <p style={{fontWeight:600,fontSize:"14px",}}>{data.text}</p>
+       </Box>
+      ))
+      }
+    </Box>
   )
 }
 
-export default Cart
+export {Cart}
---
+++ wishcart/src/components/LandingPage/LoginDialog.jsx
@@ -0,0 +1,71 @@
+import {Modal,ModalContent,Button,useDisclosure,Input,Box} from "@chakra-ui/react";
+import { useState } from "react";
+
+const accountinitialvalues={
+    login:{
+        view:"login",
+        heading:"Login",
+        subheading:"Get access to your Orders, Wishlist and Recommendations"
+
+    },
+    signup:{
+  view:"signup",
+  heading:"Looks like you're new here!",
+  subheading:"Sign up with your mobile number to get started"
+    }
+}
+  function LoginDialog() {
+   
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [account,toggleAccount]=useState(accountinitialvalues.login)
+const toggleSignup=()=>{
+    toggleAccount(accountinitialvalues.signup)
+}
+const handleClose=()=>{
+    onClose(true)
+    toggleAccount(accountinitialvalues.login)
+}
+
+
+
+    return (
+      <>
+        <p onClick={onOpen}>Login</p>
+  
+        <Modal isOpen={isOpen}  size= "xl" onClose={handleClose} >
+    
+         
+          <ModalContent style={{display:"flex",height:"500px" ,width:"50%",marginTop:"5%"}} >  
+            <Box style={{display:"flex", height:"100%"}}>      
+        <Box style={{backgroundColor:"#2874f0" , width:"50%",height:"auto",padding:"45px 35px",color:"white"}}>
+            <h1 style={{color:878787,fontSize:20}}>{account.heading}</h1>
+            <h1 style={{marginTop:20,color:878787}}>{account.subheading}</h1>
+            <img  src={"https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png"} alt={"logo"} style={{paddingTop:"80%",width:"100%"}}/>
+        </Box>
+          { account.view==="login" ?
+          <Box style={{height:"100%",width:"60%",padding:"40px 35px",textAlign:"center"}}>
+          <Input variant="flushed" placeholder="Enter Email/Mobile Number" style={{marginTop:20}}/>
+          <Input variant="flushed" placeholder="Enter Password" style={{marginTop:20}}/>
+           <p style={{marginTop:20,fontSize:12,color:878787}}>By continuing, you agree to Flipkart's Terms of Use and Privacy Policy.</p>
+            <Button style={{marginTop:20,background:"#FB641B",color:"#fff",width:200}}>Login</Button>
+            <p style={{marginTop:20,fontSize:12,color:878787}}>OR</p>
+            <Button style={{marginTop:20,background:"#FB641B",color:"#fff",width:200}}>Request OTP</Button>
+            <p style={{marginTop:20,padding:"5px",fontSize:14,color:"#2874f0"}} onClick={()=>toggleSignup()}>New to Flipkart? Create an account</p>
+          </Box>
+          : 
+          <Box style={{height:"100%",width:"60%",padding:"40px 35px",textAlign:"center"}}>
+          <Input variant="flushed" placeholder="Enter Email/Mobile Number" style={{marginTop:20}}/>
+          <Input variant="flushed" placeholder="Enter Password" style={{marginTop:20}}/>
+           
+          </Box>
+  }
+          </Box>
+       
+          </ModalContent>
+        
+        </Modal>
+      </>
+    );
+  }
+
+  export default LoginDialog;
